e22f96a0b428bd5794b135c52305ba5b18697cf7
franknstin <55328607+franknstin@users.noreply.github.com>
2022-06-23 08:43:48 +0530

    add_exception_file

--- app.py
+++ app.py
@@ -1,13 +1,20 @@
 from zlib import MAX_WBITS
 from flask import Flask
 from housing.logger import logging
+from housing.exception import HousingException
+import sys
 
 app = Flask(__name__)
 
 @app.route("/", methods=['GET', 'POST'])
 
 def index():
-    logging.info('testing logging module')
+    try:
+        raise Exception("we are testing custom exception")
+    except Exception as e:
+        housing = HousingException(e,sys)
+        logging.info(housing.error_message)
+        logging.info('testing logging module')
     return "created CI CD pipeline"
 
 
